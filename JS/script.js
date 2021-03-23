549d2a0e45ab8e251dd4999ff5febfe237ddc5e2
dfswebdesign <rdaley.cybertech@gmail.com>
2021-03-23 01:19:24 -0400

    added faq section accordion style

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,12 @@ Array.from(navLinks).forEach((link) =>
 		navButton.checked = false;
 	})
 );
+
+var $title = $(".js-title");
+var copy = ".js-copy";
+
+$title.click(function () {
+	$(this).next(copy).slideToggle();
+	$(this).parent().siblings().children().next().slideUp();
+	return false;
+});
