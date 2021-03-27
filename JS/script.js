7a0ee1ebeb4228ca332a9f812eb2c92c8ea755a1
dfswebdesign <rdaley.cybertech@gmail.com>
2021-03-26 21:09:12 -0400

    made links active

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,14 @@
-const navButton = document.getElementById("navi-toggle");
-const navLinks = document.querySelectorAll(".navigation__link");
+const navButton = document.getElementById('navi-toggle');
+const navLinks = document.querySelectorAll('.navigation__link');
 
 Array.from(navLinks).forEach((link) =>
-	link.addEventListener("click", () => {
+	link.addEventListener('click', () => {
 		navButton.checked = false;
 	})
 );
 
-var $title = $(".js-title");
-var copy = ".js-copy";
+var $title = $('.js-title');
+var copy = '.js-copy';
 
 $title.click(function () {
 	$(this).next(copy).slideToggle();
